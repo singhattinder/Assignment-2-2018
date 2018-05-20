7418f521741f55cdf929050d3fdd28c2605ce95b
Attinderpal <singh.at@husky.neu.edu>
2018-05-20 02:40:39 -0400

    course row complete

--- src/components/CourseRow.js
+++ src/components/CourseRow.js
@@ -1,8 +1,16 @@
 import React from 'react';
 class CourseRow extends React.Component {
+
+    constructor(props){
+        super(props);
+
+
+
+    }
+
     render() {
         return (
-            <tr><td>Course Row1</td></tr>
+            <tr><td>{this.props.course.title}</td></tr>
         )
     }
 }
--- src/containers/CourseList.js
+++ src/containers/CourseList.js
@@ -26,7 +26,7 @@ class CourseList extends React.Component {
               courses = this.state.courses.map(
 
                     function (course) {
-                        return <CourseRow/>
+                        return <CourseRow key={course.id} course={course}/>
                     }
                 );
 
